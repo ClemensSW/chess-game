7a43883c148670b03433ad73a107db4fe8edeaad
ClemensSW <c.walther93@gmail.com>
2025-03-02 23:29:39 +0100

    New Deploymend Test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/chess-game/",
+  base: "/chess-game/", // Add this line with your repository name
+  build: {
+    // Ensure correct file extension
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
 });
