1ce373e8ccc2670d20fdaef95cba281f5b52cf45
ClemensSW <c.walther93@gmail.com>
2025-03-02 23:39:57 +0100

    next try

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,12 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/chess-game/", // Add this line with your repository name
+  // Keine Änderung der Dateiendung nötig
   build: {
-    // Ensure correct file extension
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        },
         entryFileNames: "assets/[name].js",
         chunkFileNames: "assets/[name].js",
         assetFileNames: "assets/[name].[ext]",
